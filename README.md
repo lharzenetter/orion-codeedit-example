ba5ac5690d107f45d36681c23d75ca9dfc4700f0
Lukas Harzenetter <lharzenetter@gmx.de>
2017-10-05 15:49:12 +0200

    add example for not loading files

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
-# OrionCodeeditExample
-This repository is used to create an markdown editor + viewer. Its based on the eclipse orion codeEdit-Widget. 
+# Orion Built-Edit Angular Example
+This repository is used to create an editor for xml using the orion [built-editor in version 16.0.0S1](https://www.eclipse.org/downloads/download.php?file=/orion/drops/R-16.0-201710030941/built-editor.zip). 
 
-Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
+## Getting started
+1. run `npm install`
+2. run `ng serve`
+3. navigate to `http://localhost:4200/`.
+
+The app will automatically reload if you change any of the source files.
 
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-declare var orion: any;
+declare var System: any;
 
 @Component({
   selector: 'app-root',
@@ -11,47 +11,26 @@ export class AppComponent implements OnInit {
 
   title = 'app';
 
-  codeEdit: any;
+  orionEditor: any;
+  innerValue = `<note>
+    <to>Tove</to>
+    <from>Jani</from>
+    <heading>Reminder</heading>
+    <body>Don't forget me this weekend!</body>
+</note>
+`;
 
   constructor() {
   }
 
   ngOnInit() {
-
-    const contents = 'var foo = "bar";\n' +
-      'var bar = foo;\n' +
-      '/*\n' +
-      ' * test demo\n' +
-      '*/\n' +
-      'function test(){\n' +
-      '	var foo1 = bar.lastIndexOf(char, from);\n' +
-      '}\n' +
-      '//Keep editing in this demo and try the content assist, problem validations and hover service!\n' +
-      'var foo2 = foo.';
-
-    const defaultPluginURLs = [
-      'assets/built-codeEdit15_1/javascript/plugins/javascriptPlugin.html',
-      'assets/built-codeEdit15_1/webtools/plugins/webToolsPlugin.html',
-      'assets/built-codeEdit15_1/plugins/embeddedToolingPlugin.html'
-    ];
-
-    const userPluginURLs = [];
-    const editorBuilder = new orion.codeEdit({ _defaultPlugins: defaultPluginURLs, userPlugins: userPluginURLs });
-
-    this.codeEdit = editorBuilder.create({
-      parent: 'embeddedEditor'
-    });
-
-    this.codeEdit.then(function (editorViewer: any) {
-      document.getElementById('progressMessageDiv').textContent = 'Plugins loaded!';
-      if (editorViewer.settings) {
-        editorViewer.settings.contentAssistAutoTrigger = true;
-        editorViewer.settings.showOccurrences = true;
-        editorViewer.editor.getTextView().setOptions({ themeClass: 'src/app/app.component.css' });
-      }
-      editorViewer.setContents(contents, 'application/javascript');
-    });
-
+    System.import('../../orion/editor/built-editor.js')
+      .then((o: any) => {
+        this.orionEditor = o({
+          document: document, className: 'editor', parent: 'xml', contentType: 'application/xml'
+        })[0];
+        this.orionEditor.setText(this.innerValue);
+      });
   }
 
 }
